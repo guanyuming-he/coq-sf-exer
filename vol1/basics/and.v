82fae59a6f32764d81a7268f420d67aada64290a
Guanyuming He <guanyuminghe@outlook.com>
2025-01-01 22:28:42 +0000

    finish the vol1.basics booleans exercises

---
+++ vol1/basics/and.v
@@ -0,0 +1,14 @@
+Require booleans.
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool
+  :=
+  if andb (andb b1 b2) b3 then true
+  else false.
+Example test_andb31: (andb3 true true true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_andb32: (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_andb33: (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_andb34: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
---
+++ vol1/basics/nand.v
@@ -0,0 +1,14 @@
+Require booleans.
+
+Definition nandb (b1:bool) (b2:bool) : bool
+  :=
+  if b1 then negb b2
+  else true.
+Example test_nandb1: (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb2: (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb3: (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
