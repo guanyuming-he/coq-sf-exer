81d75f9a0d85d29841192fa57e921c233ba48374
Guanyuming He <guanyuminghe@outlook.com>
2025-02-04 13:06:24 +0000

    finish pt1 of more exercises of lists.

--- vol1/lists/defns.v
+++ vol1/lists/defns.v
@@ -61,6 +61,16 @@ Fixpoint app (l1 l2 : natlist) : natlist :=
 Notation "x ++ y" := (app x y)
                      (right associativity, at level 60).
 
+Theorem app_assoc : forall l1 l2 l3 : natlist,
+  (l1 ++ l2) ++ l3 = l1 ++ (l2 ++ l3).
+Proof.
+  intros l1 l2 l3. induction l1 as [| n l1' IHl1'].
+  - (* l1 = nil *)
+    reflexivity.
+  - (* l1 = cons n l1' *)
+  simpl. rewrite -> IHl1'. reflexivity. 
+Qed.
+
 Definition hd (default : nat) (l : natlist) : nat :=
   match l with
   | nil => default
@@ -71,3 +81,47 @@ Definition tl (l : natlist) : natlist :=
   | nil => nil
   | h :: t => t
   end.
+
+Fixpoint rev (l:natlist) : natlist :=
+  match l with
+  | nil =>  nil
+  | h :: t =>  rev t ++ [h]
+  end.
+Example test_rev1: rev [1;2;3] = [3;2;1].
+Proof. reflexivity. Qed.
+Example test_rev2: rev nil = nil.
+Proof. reflexivity. Qed.
+
+Theorem app_length_S: forall l n,
+  length (l ++ [n]) = S (length l).
+Proof.
+  intros l n. induction l as [| m l' IHl'].
+  - (* l =  *)
+    simpl. reflexivity.
+  - (* l = m:: l' *)
+    simpl.
+    rewrite IHl'.
+    reflexivity.
+Qed.
+
+Theorem app_length : forall l1 l2 : natlist,
+  length (l1 ++ l2) = (length l1) + (length l2).
+Proof.
+  (* WORKED IN CLASS *)
+  intros l1 l2. induction l1 as [| n l1' IHl1'].
+  - (* l1 = nil *)
+    reflexivity.
+  - (* l1 = cons *)
+simpl. rewrite -> IHl1'. reflexivity. Qed.
+
+Theorem rev_length : forall l : natlist,
+  length (rev l) = length l.
+Proof.
+  intros l. induction l as [| n l' IHl'].
+  - (* l = nil *)
+    reflexivity.
+  - (* l = cons *)
+    simpl. rewrite -> app_length.
+    simpl. rewrite -> IHl'. rewrite add_comm.
+    reflexivity.
+Qed.
---
+++ vol1/lists/more/pt1.v
@@ -0,0 +1,97 @@
+From LF Require Import defns list_funs.
+
+Theorem app_nil_r : forall l : natlist,
+  l ++ [] = l.
+Proof.
+  intros l.
+  induction l.
+  - reflexivity.
+  - simpl. rewrite -> IHl. reflexivity.
+Qed.
+
+
+Theorem rev_app_distr: forall l1 l2 : natlist,
+  rev (l1 ++ l2) = rev l2 ++ rev l1.
+Proof.
+  intros l1.
+  induction l1.
+  - simpl. intros l2. rewrite -> app_nil_r. reflexivity.
+  - intros l2. simpl. rewrite -> IHl1. rewrite -> app_assoc. reflexivity.
+Qed.
+
+Theorem rev_involutive : forall l : natlist,
+  rev (rev l) = l.
+Proof.
+  intros l.
+  induction l.
+  - simpl. reflexivity.
+  - simpl. rewrite -> rev_app_distr. rewrite -> IHl. reflexivity.
+Qed.
+
+Theorem app_assoc4 : forall l1 l2 l3 l4 : natlist,
+  l1 ++ (l2 ++ (l3 ++ l4)) = ((l1 ++ l2) ++ l3) ++ l4.
+Proof.
+  (** 
+     well, in general, not just for natlists, we apply the normal associativity
+     twice to get this.
+  *)
+  intros l1 l2 l3 l4.
+  rewrite -> app_assoc.
+  rewrite -> app_assoc.
+  reflexivity.
+Qed.
+
+Lemma nonzeros_app : forall l1 l2 : natlist,
+  nonzeros (l1 ++ l2) = (nonzeros l1) ++ (nonzeros l2).
+Proof.
+  intros l1.
+  induction l1.
+  - simpl. reflexivity.
+  - simpl. destruct n.
+    + simpl. exact IHl1.
+    + simpl. intros l2. rewrite -> IHl1. reflexivity.
+Qed.
+
+
+
+Fixpoint eqblist (l1 l2 : natlist) : bool
+  :=
+  match l1 with 
+  | nil => 
+      match l2 with
+      | nil => true
+      | _ :: _ => false 
+      end
+  | h :: t =>
+      match l2 with
+      | nil => false 
+      | h' :: t' =>
+        if h =? h' then eqblist t t'
+        else false 
+      end
+   end.
+Example test_eqblist1 :
+  (eqblist nil nil = true).
+Proof. reflexivity. Qed.
+
+Example test_eqblist2 :
+  eqblist [1;2;3] [1;2;3] = true.
+Proof. reflexivity. Qed.
+
+Example test_eqblist3 :
+  eqblist [1;2;3] [1;2;4] = false.
+Proof. reflexivity. Qed.
+
+Theorem eqblist_refl : forall l:natlist,
+  true = eqblist l l.
+Proof.
+  intros l.
+  induction l.
+  - reflexivity.
+  - simpl. assert (forall n: nat, n =? n = true).
+    { intros n0. induction n0. 
+      + reflexivity.
+      + simpl. rewrite -> IHn0. reflexivity.
+    }
+    rewrite -> H. apply IHl.
+Qed.
